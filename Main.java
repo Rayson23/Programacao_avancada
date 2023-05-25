1389ed1fff7dcb049e45354b9c3bd8863ea774b6
Rayson23 <134613090+Rayson23@users.noreply.github.com>
2023-05-25 09:44:10 -0300

    pilha

--- Main.java
+++ Main.java
@@ -1,24 +1,39 @@
 /******************************************************************************
 
+30/03 Cilene - TESTAR A PILHA PARA RESOLVER EXPRESSÃO MATEMÁTICA
 
 *******************************************************************************/
 public class Main
 {
 	public static void main(String[] args) {
-	   mensagem();
-	   mensagem("que legal");
-	   mensagem("cada enxadada uma minhoca");
-	   mensagem("cada enxadada uma minhoca ","cada enxadada uma minhoca");
-	}
-	public static void mensagem(){
-	    	System.out.println("Hello World");
-	}
-	public static void mensagem(String m){
-	    	System.out.println(m);
-	    
-	}
-		public static void mensagem(String m, String n){
-	    	System.out.println(m+n);
-	    
+		// Criar uma pilha p - chamando o CONSTRUTOR
+		TADPilha p = new TADPilha (100);
+		
+		String expressao="2345+*6+*"; //"2345+*6+*";
+		char aux;
+		int a,b,res=0;
+		
+		for (int i=0; i< expressao.length(); i++)  {
+		    aux = expressao.charAt(i);
+		    // Se for numero, empilha
+		    
+		    int c = Character.getNumericValue(aux);
+		    if (aux>='0' && aux <= '9') p.empilha(c);
+		    // se for operacao, 
+		    //   desempilha 2 (a e b)
+		    //   calcula a operacao e empilha o resultado
+		    else {
+		         a =  p.desempilha();
+		         b =  p.desempilha();
+		        if (aux=='+') res= a+b;
+		        if (aux=='-') res= a-b;
+		        if (aux=='*') res= a*b;
+		        if (aux=='/') res= a/b;
+		       p.empilha(res);
+		    }
+		}
+        res = p.desempilha();
+        System.out.println ("Resultado=" + res);
+
 	}
 }
--- TADPilha.java
+++ TADPilha.java
@@ -1,48 +1,61 @@
-/***********************************************
-Classe Pilha 30/03 
-************************************************/
-public class TADPilha{
+/******************************************************************************
+30/03 Cilene - CLASSE TADPilha
+*******************************************************************************/
+public class TADPilha {
+    // atributos
     int dados[];
     int topo;
     int capacidade;
     
-    public TADPilha(int c){
-    capacidade = c;
-    topo = -1;
-    dados = new int [capacidade];
+    // Construtor 
+    public TADPilha (int c) {
+       capacidade = c;
+       topo = -1;
+       dados = new int [capacidade];
     }
-    
-    public boolean empilha(int x){
-        if(!cheia()){
-        topo++;
-        dados[topo] = x;
-        return true;
+
+    // métodos
+    public boolean empilha(int x) {
+        if (!cheia()) {
+           topo++;  // topo = topo + 1;
+           dados[topo] = x;
+           return true;
         }
-        System.out.println("cheia !");
+        System.out.print ("cheia !");
         return false;
     }
-    
-    public void mostraAoContrario(){
-        for(int i=topo;i>=0;i--){
-            System.out.println(dados[i]);
+    public int desempilha() {
+        int aux=0;
+        if (!vazia()) {
+            aux = dados[topo];
+            topo--;  // topo = topo + 1;
         }
+        return aux;
     }
-     public void mostra(){
-        for(int i=0;i<=topo;i++){
-            System.out.println(dados[i]);
-        }
-    }
+
+    public void mostraAoContrario () {
+       for (int i=topo; i>=0; i--) 
+           System.out.println (dados[i]);    
+    } 
+    public void mostra () {
+       for (int i=0; i<=topo; i++) 
+           System.out.println (dados[i]);    
+    } 
     
-    public boolean cheia(){
+    public boolean cheia() {
         return (topo==capacidade-1);
-        
     }
-    public boolean vazia(){
+    public boolean vazia() {
         return (topo==-1);
     }
-    public int conta(){
+    public int conta () {
         return topo+1;
     }
     
     
-}
+    
+    
+    
+    
+    
+ }
